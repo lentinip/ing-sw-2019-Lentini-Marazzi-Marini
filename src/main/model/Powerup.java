6306280927d5d4621a85992b9388466f8d55994b
Mi97ch <michele1.marazzi@mail.polimi.it>
2019-03-31 18:21:22 +0200

    insert classes skeleton

---
+++ src/main/model/Powerup.java
@@ -0,0 +1,44 @@
+package model;
+
+
+public class Powerup {
+
+    /* Default constructor */
+
+    public Powerup() {
+
+    }
+
+    /* Attributes */
+
+    private String name;
+
+    private Colors color;
+
+    /* Methods */
+
+    public String getName(){
+        return(name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Colors getColor() {
+        return(color);
+    }
+
+    public Colors setColor(Colors color) {
+        this.color = color;
+    }
+
+    public void effect() {
+
+        //TODO implement
+
+        return;
+    }
+
+
+}
