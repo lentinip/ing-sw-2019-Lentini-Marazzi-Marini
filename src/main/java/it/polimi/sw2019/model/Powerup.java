a774f9ee60bcae8f592f529d146a1716de9f9253
Mi97ch <michele1.marazzi@mail.polimi.it>
2019-03-31 23:53:48 +0200

    Added classes to model package

---
+++ src/main/java/it/polimi/sw2019/model/Powerup.java
@@ -0,0 +1,44 @@
+package java.it.polimi.sw2019.model;
+
+
+public class Powerup {
+
+    /* Default constructor */
+
+    public Powerup() {
+
+    }
+
+    /* Attributes */
+
+    private String name;
+
+    private Colors color;
+
+    /* Methods */
+
+    public String getName(){
+        return(name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Colors getColor() {
+        return(color);
+    }
+
+    public Colors setColor(Colors color) {
+        this.color = color;
+    }
+
+    public void effect() {
+
+        //TODO implement
+
+        return;
+    }
+
+
+}
---
+++ src/main/java/it/polimi/sw2019/model/Weapon.java
@@ -0,0 +1,61 @@
+package java.it.polimi.sw2019.model;
+
+public class Weapon {
+
+    /** Default constructor **/
+
+    public Weapon() {
+
+    }
+
+    /** Attributes **/
+
+    private String name;
+
+    private Ammo grabCost;
+
+    private Ammo reloadCost;
+
+    private boolean isLoaded;
+
+    /** Methods **/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public Ammo getGrabCost() {
+        return grabCost;
+    }
+
+    public void setGrabCost(Ammo grabCost) {
+        this.grabCost = grabCost;
+    }
+
+    public Ammo getReloadCost() {
+        return reloadCost;
+    }
+
+    public void setReloadCost(Ammo grabCost) {
+        this.reloadCost = reloadCost;
+    }
+
+    public boolean getIsLoaded() {
+        return isLoaded;
+    }
+
+    public void setIsLoaded(boolean isLoaded) {
+        this.isLoaded = isLoaded;
+    }
+
+    public void effect() {  // use the weapon effect
+
+        //TODO implement
+
+        return null;
+    }
+}
